0d6a45b15d1626b6cd0575f82ef1a567bec3b2a6
Thalitalima23 <thalitalima23@gmail.com>
2025-10-17 15:33:31 -0300

    :tada: Commit Inicial

---
+++ src/main/java/com/generation/lojadegames/model/Categoria.java
@@ -0,0 +1,74 @@
+package com.generation.lojadegames.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	 @NotBlank(message = "O nome da categoria é obrigatório!")
+	 @Size(min = 4, max = 100, message= "O atributo nome deve conter no mínimo 4 e no máximo 100 caracteres")
+	 private String nome;
+	 
+	 @NotBlank(message = "A descrição é obrigatória!")
+	 @Size(min = 6, max = 100, message= "O produto deve conter uma descrição de no mínimo 6 caracteres e no máximo 100 caracteres ")
+	 private String descricao;
+	 
+	  // Criando o Relacionamento com a Classe Postagem!
+	 
+	   @OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	    @JsonIgnoreProperties("categoria")
+	    private List<Produto> produto;
+
+	   	//Getters and Setters
+	   	
+	   public Long getId() {
+		   return id;
+	   }
+
+	   public void setId(Long id) {
+		   this.id = id;
+	   }
+
+	   public String getNome() {
+		   return nome;
+	   }
+
+	   public void setNome(String nome) {
+		   this.nome = nome;
+	   }
+
+	   public String getDescricao() {
+		   return descricao;
+	   }
+
+	   public void setDescricao(String descricao) {
+		   this.descricao = descricao;
+	   }
+
+	   public List<Produto> getProduto() {
+		   return produto;
+	   }
+
+	   public void setProduto(List<Produto> produto) {
+		   this.produto = produto;
+	   }
+	   
+	
+}
---
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,101 @@
+package com.generation.lojadegames.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos") 
+public class Produto {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(length = 100)
+    @NotBlank(message = "O nome da produto é obrigatório!")
+    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres")
+    private String nome;
+	
+	@Column(length = 1000)
+    @NotBlank(message = "O atributo descrição é obrigatório!")
+    @Size(min = 10, max = 1000, message = "A descrição deve ter entre 10 e 1000 caracteres")
+    private String descricao;
+	
+    @NotNull(message = "O preço é obrigatório!")
+    private BigDecimal preco;
+    
+    @NotBlank(message = "O atributo plataforma é obrigatório!")
+    private String plataforma; // Exemplo: "PC", "PlayStation", "Xbox", "Switch"
+	
+    // Relacionamento N:1 com a classe Postagem
+    
+    @ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+    
+    // Getters e Setters
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+    
+
+    
+    
+
+}
