0472d79a1e196e55ec5e14f67c876f45d74097d8
Thalitalima23 <thalitalima23@gmail.com>
2025-10-21 15:48:09 -0300

    :sparkles: Implementação da Security

---
+++ src/main/java/com/generation/lojadegames/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.generation.lojadegames.repository;
+
+	import java.util.Optional;
+
+	import org.springframework.data.jpa.repository.JpaRepository;
+
+	import com.generation.blogpessoal.model.Usuario;
+
+	public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+		Optional<Usuario> findByUsuario(String usuario);
+
+	}
+
